6f74493eca906816c84b2a7c87ead87701fec3c3
Claudiu Marginean <claudiu.marginean@mindmagnetsoftware.com>
2014-09-14 16:06:43 +0300

    Init module files

---
+++ app/code/community/Hackathon/TemplateHintsHover/Model/Observer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of the TemplateHintsHover project.
+ *
+ * Hackathon TemplateHintsHover is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  TemplateHintsHover
+ * @package   Hackathon_TemplateHintsHover
+ * @author    Claudiu Marginean <claudiu.f.marginean@gmail.com>
+ * @author    Mihai MATEI <mihaimatei@gmail.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version   0.1.0
+ */
+
+/**
+ * Observer
+ *
+ * @category  TemplateHintsHover
+ * @package   Hackathon_TemplateHintsHover
+ * @author    Claudiu Marginean <claudiu.f.marginean@gmail.com>
+ * @author    Mihai MATEI <mihaimatei@gmail.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version   0.1.0
+ */
+class Hackathon_TemplateHintsHover_Model_Observer
+{
+
+    /**
+     * Add Template Hints
+     *
+     * @param Varien_Event_Observer $observer
+     *
+     */
+    public function addTemplateHints(Varien_Event_Observer $observer)
+    {
+
+    }
+}
